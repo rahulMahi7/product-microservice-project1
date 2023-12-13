383962c290f4a3c8c4d5a33e4cb3d4bd32000dfb
rahulMahi7 <chhetrirahul77777@gmail.com>
2023-12-13 21:09:46 +0530

    URI bug fix for acceesing through the api getway

--- api-gateway/src/main/java/org/microservices/ApiGatewayApplication.java
+++ api-gateway/src/main/java/org/microservices/ApiGatewayApplication.java
@@ -1,10 +1,13 @@
 package org.microservices;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
