b57d3f193208be92876456e40e2200cd41691a3c
rahulMahi7 <chhetrirahul77777@gmail.com>
2023-12-13 14:00:27 +0530

    Added api-gateway for microservices and defined routes for different microservices

---
+++ api-gateway/src/main/java/org/microservices/ApiGatewayApplication.java
@@ -0,0 +1,10 @@
+package org.microservices;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
